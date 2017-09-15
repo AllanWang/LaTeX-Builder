751b1a5d4013cce7d4e6eb8eb4db831282261fb3
AllanWang <me@allanwang.ca>
2017-09-15 13:26:05 -0400

    Add travis builder

--- latex-builder.sh
+++ latex-builder.sh
@@ -12,13 +12,15 @@ pdflatex -output-directory _latex_build -jobname=generated ${TEX_PATH}
 
 echo "Create New Release\n"
 
-API_JSON="$(printf '{"tag_name": "v%s","target_commitish": "master","name": "v%s","body": "Automatic Release v%s","draft": false,"prerelease": false}' $TRAVIS_BUILD_NUMBER $TRAVIS_BUILD_NUMBER $TRAVIS_BUILD_NUMBER)"
+API_JSON="$(printf '{"tag_name": "v%s","target_commitish": "master","name": "v%s","body": "Automatic Release v%s","draft": false,"prerelease": false}' \
+  $TRAVIS_BUILD_NUMBER $TRAVIS_BUILD_NUMBER $TRAVIS_BUILD_NUMBER)"
 newRelease="$(curl --data "$API_JSON" https://api.github.com/repos/$TRAVIS_REPO_SLUG/releases?access_token=$GITHUB_API_KEY)"
 rID="$(echo "$newRelease" | jq ".id")"
 
 
 
 echo "Push pdf to $rID"
-curl "https://uploads.github.com/repos/${TRAVIS_REPO_SLUG}/releases/${rID}/assets?access_token=${GITHUB_API_KEY}&name=${PDF_NAME}-v${TRAVIS_BUILD_NUMBER}.pdf" --header 'Content-Type: application/pdf' --upload-file _latex_build/generated.pdf -X POST
+curl "https://uploads.github.com/repos/${TRAVIS_REPO_SLUG}/releases/${rID}/assets?access_token=${GITHUB_API_KEY}&name=${PDF_NAME}-v${TRAVIS_BUILD_NUMBER}.pdf" \
+  --header 'Content-Type: application/pdf' --upload-file _latex_build/generated.pdf -X POST
 
 echo -e "Done\n"
